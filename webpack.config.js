a59bd4aa7830ce724822a3b2355add0ce425379d
Ilya Begalko <ilja.begalko@yandex.ru>
2023-07-15 14:01:21 +0600

    setting DevServer Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,11 @@ module.exports = {
     mode,
     target,
     devtool,
+    devServer: {
+        port: 3000,
+        open: true,
+        hot: true 
+    },
     entry: path.resolve(__dirname, 'src', 'index.js'),
     output: {
         path: path.resolve(__dirname, 'dist'),
